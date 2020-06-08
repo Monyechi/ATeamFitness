4fb8375bf4cd59b3eb60f44a76ad0f6ba2006b06
Monyechi <63468925+Monyechi@users.noreply.github.com>
2020-06-08 13:06:54 -0400

    added logic to dbcontext

--- ATeamFitness/Startup.cs
+++ ATeamFitness/Startup.cs
@@ -12,6 +12,9 @@ using ATeamFitness.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using ATeamFitness.ActionFilters;
 
 namespace ATeamFitness
 {
@@ -35,6 +38,13 @@ namespace ATeamFitness
             .AddEntityFrameworkStores<ApplicationDbContext>()
             .AddDefaultUI()
             .AddDefaultTokenProviders();
+
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
