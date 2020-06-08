a5e4be259ddac47e57cfeb184686a17e4a636bb5
Monyechi <63468925+Monyechi@users.noreply.github.com>
2020-06-08 12:48:53 -0400

    Created customer model

---
+++ ATeamFitness/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATeamFitness.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public int StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string FitnessGoal { get; set; }
+        public string FitnessPlan { get; set; }
+        public string DietPlan { get; set; }
+        public int RewardPoint { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
